a59902dcddf7701d554809a47be00adea2dce062
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-04 15:11:50 -0500

    add fetch syntax, select DOM elements, create favorite btn event listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('hello')
+//DOM elements/ variables
+const url = 'http://127.0.0.1:2000/pokemons'
+const favoriteBtn = document.querySelector('#favorite-poke')
+const newPokeForm = document.querySelector('#poke-form')
+
+//fetches
+
+// //post fetch
+// fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+//   })
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+
+
+
+
+
+//eventlisteners
+//  favoriteBtn.addEventListener('click', toggle)
+
+
+
+//render functions
+
+
+
+//callback functions
+// // function toggle(e){
+//     // e.target.textContent =! e.target.textContent
+//     // e.target.textContent = e.target.textContent ? "Favorite" : "Not Favorite"
+    
+// }
+
