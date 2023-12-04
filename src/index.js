2fbedaf815cbbef789a9b15b8e3771666532f89b
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-12-04 15:53:18 -0500

    GET request and map

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const url = ' http://localhost:3000/pokemons'
 const favoriteBtn = document.querySelector('#favorite-poke')
 const newPokeForm = document.querySelector('#poke-form')
 
+const pokeDetailName = document.getElementById("name")
+const pokeDetailImage = document.getElementById("poke-image")
+const pokeDetailType = document.getElementById("type")
+const pokedexDetailNumber = document.getElementById("pokedex")
+
 //fetches
 fetch(url)
 .then(response => response.json())
@@ -10,7 +15,27 @@ fetch(url)
     pokeData.map(eachPoke => {
         addPokeToPage(eachPoke)
     })
+    pokeDetails(pokeData[0])
 })
+
+function addPokeToPage(pokemon){
+    const pokeList = document.getElementById("pokemon-list")
+    const pokeImage = document.createElement("img")
+    pokeImage.src = pokemon.image 
+    pokeList.appendChild(pokeImage)
+
+    pokeImage.addEventListener("click", () => {
+        pokeDetails(pokemon)
+    })
+
+}
+
+function pokeDetails(pokemon){
+    pokeDetailName.textContent = pokemon.name
+    pokeDetailImage.src = pokemon.image 
+    pokeDetailType.textContent = pokemon.type
+    pokedexDetailNumber.textContent = pokemon.pokedex
+}
 // //post fetch
 // fetch(url, {
 //     method: "POST",
