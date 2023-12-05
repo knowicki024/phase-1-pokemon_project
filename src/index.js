c756eb693ec68e2659d41741d8c016ba233280a8
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-04 19:04:19 -0500

    refactored codes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ const url = ' http://localhost:3000/pokemons'
 const favoriteBtn = document.querySelector('#favorite-poke')
 const newPokeForm = document.querySelector('#poke-form')
 
-
 const pokeDetailName = document.getElementById("name")
 const pokeDetailImage = document.getElementById("poke-image")
 const pokeDetailType = document.getElementById("type")
 const pokedexDetailNumber = document.getElementById("pokedex")
 const pokeCollection = document.getElementById("collection_amount")
-
-
 const deleteBtn = document.getElementById('delete-poke')
 const pokeInfoDiv = document.getElementById('pokemon-info')
 
@@ -24,25 +21,6 @@ fetch(url)
     pokeDetails(pokeData[0])
 })
 
-function addPokeToPage(pokemon){
-    const pokeList = document.getElementById("pokemon-list")
-    const pokeImage = document.createElement("img")
-    pokeImage.src = pokemon.image 
-    pokeList.appendChild(pokeImage)
-
-    pokeImage.addEventListener("click", () => {
-        pokeDetails(pokemon)
-    })
-
-}
-
-function pokeDetails(pokemon){
-    pokeDetailName.textContent = pokemon.name
-    pokeDetailImage.src = pokemon.image 
-    pokeDetailType.textContent = pokemon.type
-    pokedexDetailNumber.textContent = pokemon.pokedex
-    pokeCollection.textContent = pokemon.collection_amount
-}
 // //post fetch
 // fetch(url, {
 //     method: "POST",
@@ -56,17 +34,29 @@ function pokeDetails(pokemon){
 
 
 
-
-
-
 //eventlisteners
 //  favoriteBtn.addEventListener('click', toggle)
 deleteBtn.addEventListener('click', handleDelete)
 
 
 //render functions
+function pokeDetails(pokemon){
+    pokeDetailName.textContent = pokemon.name
+    pokeDetailImage.src = pokemon.image 
+    pokeDetailType.textContent = pokemon.type
+    pokedexDetailNumber.textContent = pokemon.pokedex
+    pokeCollection.textContent = pokemon.collection_amount
+}
+function addPokeToPage(pokemon){
+    const pokeList = document.getElementById("pokemon-list")
+    const pokeImage = document.createElement("img")
+    pokeImage.src = pokemon.image 
+    pokeList.appendChild(pokeImage)
 
-
+    pokeImage.addEventListener("click", () => {
+        pokeDetails(pokemon)
+    })
+}
 
 //callback functions
 // // function toggle(e){
